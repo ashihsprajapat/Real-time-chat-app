0fc37357747152088ec901eff27dd7ed3e4a1a5b
ashish prajapat <155178279+ashihsprajapat@users.noreply.github.com>
2025-03-19 08:08:37 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:  import.meta.MODE === "developement"? "http://localhost:8080/api":"/api",
+    baseURL:  import.meta.MODE === "developement"? "http://localhost:8080/api":"https://real-time-chat-app-1-nxe6.onrender.com",
     withCredentials: true,
 })
