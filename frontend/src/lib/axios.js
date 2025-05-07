dfd531c57d4fe083fc281fa289483229c3cea3a8
ashish prajapat <155178279+ashihsprajapat@users.noreply.github.com>
2025-05-07 08:59:27 +0530

    backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-i3umyxzdc-ashish-prajapats-projects.vercel.app/" : "https://real-time-chat-i3umyxzdc-ashish-prajapats-projects.vercel.app/",
     withCredentials: true,
 })
