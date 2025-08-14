3b72dfc71b552e28a2516bd0aed1d5026d5566ac
ashish prajapat <ashishprajapat507@gmail.com>
2025-08-14 09:26:59 +0530

    add backend url in fronend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_MODE === "development" ? "http://localhost:8080/api" : "https://real-time-chat-app-wheat-five.vercel.app",
+    baseURL: import.meta.env.VITE_MODE === "development" ? "http://localhost:8080/api" : "https://real-time-chat-app-backend-sage.vercel.app",
     withCredentials: true,
 })
