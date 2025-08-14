34f01f00106796762d5e871c4a5003eb750e8f57
ashish prajapat <ashishprajapat507@gmail.com>
2025-08-14 08:47:55 +0530

    add .env file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api" : "/api",
+    baseURL: import.meta.env.VITE_MODE === "development" ? "http://localhost:8080/api" : "https://real-time-chat-app-wheat-five.vercel.app",
     withCredentials: true,
 })
